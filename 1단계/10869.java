c3b4aac331e2e99fb0cd98e7d415727e7dd2fc51
KimJuHongg <98217140+KimJuHongg@users.noreply.github.com>
2022-07-04 20:52:47 +0900

    Create 10869.java

---
+++ 1단계/10869.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    int a,b;
+    Scanner scanner = new Scanner(System.in);
+    a = scanner.nextInt();
+    b = scanner.nextInt();
+    System.out.println(a+b);
+    System.out.println(a-b);
+    System.out.println(a*b);
+    System.out.println(a/b);
+    System.out.println(a%b);
+  }
+}
