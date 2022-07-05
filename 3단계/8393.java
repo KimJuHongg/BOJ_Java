ea2a39c0d4e8706315db1245767f98c43bd7af7e
KimJuHongg <98217140+KimJuHongg@users.noreply.github.com>
2022-07-05 23:15:52 +0900

    Create 8393.java

---
+++ 3단계/8393.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      int num = scanner.nextInt();
+      int sum = 0;
+      for(int i = 1; i <= num; i++) {
+        sum = sum + i;
+      }   
+      System.out.print(sum);
+  }
+}
